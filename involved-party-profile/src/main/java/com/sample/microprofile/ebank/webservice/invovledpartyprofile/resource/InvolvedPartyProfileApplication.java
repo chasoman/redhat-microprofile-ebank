acf3104c0cc946347c30e9bddbb7a854385005c2
stackuser <stackuser@localhost.localdomain>
2019-12-05 16:35:35 +0530

    Added second guided lab for injecting config data in a MicroProfile microservice

---
+++ involved-party-profile/src/main/java/com/sample/microprofile/ebank/webservice/invovledpartyprofile/resource/InvolvedPartyProfileApplication.java
@@ -0,0 +1,9 @@
+package com.sample.microprofile.ebank.webservice.invovledpartyprofile.resource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/involved-party-profile")
+public class InvolvedPartyProfileApplication extends Application {
+	// Class body left blank intentionally
+}
